4834e998b237a9cc513c5ac2f9581497df46e65e
hiren14 <hiren14lalni@gmail.com>
2024-02-21 16:45:26 +0530

    ADD query

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,16 +1,17 @@
-import { Body, Controller,Delete,Get, Param, Post,Patch } from '@nestjs/common';
+import { Body, Controller,Delete,Get, Param, Post,Patch,Query } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { getTasksFilterDto } from './dto/get-tasks-filiter.dto';
 
 @Controller('task')
 export class TaskController {
     constructor(private taskService:TaskService){}
 
-    @Get()
-    getAllTask() :Task[]{
-        return this.taskService.getAllTask();
-    }
+    // @Get()
+    // getAllTask() :Task[]{
+    //     return this.taskService.getAllTask();
+    // }
 
     @Get('/:id')
     
@@ -37,6 +38,17 @@ export class TaskController {
         @Body('status') status: TaskStatus,
     ){
         return this.taskService.updatedTaskStatus(id,status)
+    }
+    @Get()
+    getTasks(@Query() filterDta: getTasksFilterDto): Task[] {
+       if(Object.keys(filterDta).length){
+        return this.taskService.getTaskWithFilter(filterDta)
+       }
+       else{
+        return this.taskService.getAllTask()
+
+       }
+       
     }
 }
 
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { getTasksFilterDto } from './dto/get-tasks-filiter.dto';
 @Injectable()
 export class TaskService {
   private  task = []
@@ -32,5 +33,19 @@ export class TaskService {
       const uptask = this.getTaskById(id);
       uptask.status = status;
       return uptask
+  }
+  getTaskWithFilter(filiterDto:getTasksFilterDto):Task[]{
+    const {status,search} = filiterDto;
+    let tasks = this.getAllTask();
+    if(status){
+      tasks = tasks.filter(task => task.status === status)
+    }
+    if(search){
+      tasks =tasks.filter(task => 
+        task.title.includes(search)||
+        task.description.includes(search),  
+        );
+    }
+    return tasks
   }
 }
