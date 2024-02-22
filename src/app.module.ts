8c8bcbefbb7621e42d02d00cb0b5bb4cdd118b3b
hiren14 <hiren14lalni@gmail.com>
2024-02-22 16:32:52 +0530

    add error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
 // import { TaskController } from './task/task.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema';
 
 @Module({
-  imports: [TaskModule],
-  // controllers: [TaskController],
- 
+  imports: [TaskModule, AuthModule,
+  PassportModule.register({defaultStrategy:"jwt"}),
+  JwtModule.registerAsync({
+
+  }),
+  MongooseModule.forFeature([{name:'User',schema:UserSchema}])
+  ],
+   controllers: [AuthController],
+ providers:[AuthService]
 })
 export class AppModule {}
